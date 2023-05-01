496bcde0d893890c20fa6cbd7946428544bec6a6
Tina Shi <init.defaultBranch>
2023-05-01 17:36:08 +1000

    commit tree 59d00f7181153d9505427e29b2262293db55dfa7

--- app.rb
+++ app.rb
@@ -4,9 +4,9 @@
 #     { id: 4, hello: "Learner" }]
 
 # p [1,2,3]
-# puts "Hello World!"
-# print "Pass this test, please."
-# p [1,2,3]
+puts "Hello World!"
+print "Pass this test, please."
+p [1,2,3]
 
 # def add
     
@@ -14,10 +14,10 @@
 # # 1 + 1
 # puts add + add
 
-def add num1, num2
-    puts num1
-    puts num2
-    num1 + num2
-end
+# def add num1, num2
+#     puts num1
+#     puts num2
+#     num1 + num2
+# end
 
-puts add(1,3) + add(2,2)
+# puts add(1,3) + add(2,2)
